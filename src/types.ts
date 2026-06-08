5780430c29e80164b5581a441ac00e1a4e67204d
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-06-08 22:16:31 +0600

    9th commit

--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,8 @@ import { ServerResponse,IncomingMessage} from "http";
 export type Res = ServerResponse
 export type Req = IncomingMessage
 
+export type Method = "GET"|"DELETE"|"POST"|"PUT";
+
 
 export interface order{
     id: string
